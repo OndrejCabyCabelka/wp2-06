3d322c6394cfeed9ed871e7fdf20827c2a0b488a
Your Name <you@example.com>
2022-01-05 14:05:50 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $game[0]['name'] = 'CS:GO';
+    $game[0]['price'] = 12;
+    $game[0]['description'] = 'Dobrá hra';
+
+    $people[0]['firstname'] = 'Ondřej Čábelka'
+    $people[0]['game']
+    for ($i=0; $i < count($game); $i++) { 
+        
+            <h2><?= $people[$i]['name']; ?></h2>
+            <p><strong>Cena:</strong><?= $game[i]['price']; ?></p>
+            <p><strong>Popis:</strong><?= $game[i]['descriprion']; ?></p>
+              
+    }
+    ?>
+</body>
+</html>
